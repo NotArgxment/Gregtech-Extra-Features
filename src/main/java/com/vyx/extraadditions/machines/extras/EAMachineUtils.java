1ec722faed42d5aa24a6fac08a7f092d89f5e934
Usuario <therealxkorpion@gmail.com>
2026-05-24 23:25:15 -0300

    Thanks phoenixvine for fixing it

--- src/main/java/com/vyx/extraadditions/machines/extras/EAMachineUtils.java
+++ src/main/java/com/vyx/extraadditions/machines/extras/EAMachineUtils.java
@@ -1,33 +1,37 @@
+package com.vyx.extraadditions.machines.extras;
+
 import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
 import com.gregtechceu.gtceu.api.machine.MultiblockMachineDefinition;
 import com.gregtechceu.gtceu.api.machine.multiblock.MultiblockControllerMachine;
 import com.gregtechceu.gtceu.api.registry.registrate.GTRegistrate;
 import com.gregtechceu.gtceu.api.registry.registrate.MultiblockMachineBuilder;
+import com.vyx.extraadditions.ExtraAdditionsCore;
 
 import java.util.Locale;
 import java.util.function.BiFunction;
 
-import static com.vyx.extraadditions.ExtraAdditionsCore.EAREGISTRATE;
+public class EAMachineUtils {
 
-public static MultiblockMachineDefinition[] registerTieredMultis(String name,
-                                                                 BiFunction<IMachineBlockEntity, Integer, MultiblockControllerMachine> factory,
-                                                                 BiFunction<Integer, MultiblockMachineBuilder<?, ?>, MultiblockMachineDefinition> builder,
-                                                                 int... tiers) {
-    return registerTieredMultis(EAREGISTRATE, name, factory, builder, tiers);
-}
-
-public static MultiblockMachineDefinition[] registerTieredMultis(GTRegistrate registrate, String name,
-                                                                 BiFunction<IMachineBlockEntity, Integer, MultiblockControllerMachine> factory,
-                                                                 BiFunction<Integer, MultiblockMachineBuilder<?, ?>, MultiblockMachineDefinition> builder,
-                                                                 int... tiers) {
-    MultiblockMachineDefinition[] definitions = new MultiblockMachineDefinition[GTValues.TIER_COUNT];
-    for (int tier : tiers) {
-        var register = registrate
-                .multiblock(GTValues.VN[tier].toLowerCase(Locale.ROOT) + "_" + name,
-                        holder -> factory.apply(holder, tier))
-                .tier(tier);
-        definitions[tier] = builder.apply(tier, register);
+    public static MultiblockMachineDefinition[] TieredMultis(String name,
+                                                                     BiFunction<IMachineBlockEntity, Integer, MultiblockControllerMachine> factory,
+                                                                     BiFunction<Integer, MultiblockMachineBuilder<?, ?>, MultiblockMachineDefinition> builder,
+                                                                     int... tiers) {
+        return TieredMultis(ExtraAdditionsCore.EAREGISTRATE, name, factory, builder, tiers);
     }
-    return definitions;
+
+    public static MultiblockMachineDefinition[] TieredMultis(GTRegistrate registrate, String name,
+                                                                     BiFunction<IMachineBlockEntity, Integer, MultiblockControllerMachine> factory,
+                                                                     BiFunction<Integer, MultiblockMachineBuilder<?, ?>, MultiblockMachineDefinition> builder,
+                                                                     int... tiers) {
+        MultiblockMachineDefinition[] definitions = new MultiblockMachineDefinition[GTValues.TIER_COUNT];
+        for (int tier : tiers) {
+            var register = registrate
+                    .multiblock(GTValues.VN[tier].toLowerCase(Locale.ROOT) + "_" + name,
+                            holder -> factory.apply(holder, tier))
+                    .tier(tier);
+            definitions[tier] = builder.apply(tier, register);
+        }
+        return definitions;
+    }
 }
