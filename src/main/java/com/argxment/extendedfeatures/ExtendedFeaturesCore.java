d630913eb2f3daa66a298c6a75ebe65255b9a862
Usuario <therealxkorpion@gmail.com>
2026-06-27 15:14:53 -0300

    Code cleanup

--- src/main/java/com/argxment/extendedfeatures/ExtendedFeaturesCore.java
+++ src/main/java/com/argxment/extendedfeatures/ExtendedFeaturesCore.java
@@ -2,10 +2,12 @@ package com.argxment.extendedfeatures;
 
 import com.argxment.extendedfeatures.init.Multiblocks;
 import com.argxment.extendedfeatures.init.Items;
-import com.gregtechceu.gtceu.common.data.GTBlocks;
-import com.gregtechceu.gtceu.common.data.GTCreativeModeTabs;
+import com.argxment.extendedfeatures.client.RecipeTypes;
+
 import com.tterrag.registrate.util.entry.RegistryEntry;
 
+import com.gregtechceu.gtceu.common.data.GTBlocks;
+import com.gregtechceu.gtceu.common.data.GTCreativeModeTabs;
 import com.gregtechceu.gtceu.api.GTCEuAPI;
 import com.gregtechceu.gtceu.api.data.chemical.material.event.*;
 import com.gregtechceu.gtceu.api.machine.MachineDefinition;
@@ -13,10 +15,9 @@ import com.gregtechceu.gtceu.api.recipe.GTRecipeType;
 import com.gregtechceu.gtceu.api.registry.registrate.GTRegistrate;
 import com.gregtechceu.gtceu.api.sound.SoundEntry;
 
-// import com.vyx.extraadditions.machines.client.parallel.ExtraParaHatches;
-import com.argxment.extendedfeatures.init.utils.client.RecipeTypes;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
+
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -65,19 +66,11 @@ public class ExtendedFeaturesCore {
         modEventBus.addGenericListener(MachineDefinition.class, this::registerMachines);
         modEventBus.addGenericListener(SoundEntry.class, this::registerSounds);
 
-        // Most other events are fired on Forge's bus.
-        // If we want to use annotations to register event listeners,
-        // we need to register our object like this!
         MinecraftForge.EVENT_BUS.register(this);
 
     }
 
-    /**
-     * Create a ResourceLocation in the format "modid:path"
-     *
-     * @param path
-     * @return ResourceLocation with the namespace of your mod
-     */
+
     public static ResourceLocation id(String path) {
         return new ResourceLocation(ExtendedFeaturesCore.MOD_ID, path);
     }
@@ -93,62 +86,27 @@ public class ExtendedFeaturesCore {
         LOGGER.info("i hate java!!!!!!");
     }
 
-    /**
-     * Create a material manager for your mod using GT's API.
-     * You MUST have this if you have custom materials.
-     * Remember to register them not to GT's namespace, but your own.
-     *
-     * @param event
-     */
+
     private void addMaterialRegistries(MaterialRegistryEvent event) {
         GTCEuAPI.materialManager.createRegistry(ExtendedFeaturesCore.MOD_ID);
     }
 
-    /**
-     * You will also need this for registering custom materials
-     * Call init() from your Material class(es) here
-     *
-     * @param event
-     */
     private void addMaterials(MaterialEvent event) {
         // CustomMaterials.init();
     }
 
-    /**
-     * (Optional) Used to modify pre-existing materials from GregTech
-     *
-     * @param event
-     */
     private void modifyMaterials(PostMaterialEvent event) {
         // CustomMaterials.modify();
     }
 
-    /**
-     * Used to register your own new RecipeTypes.
-     * Call init() from your RecipeType class(es) here
-     *
-     * @param event
-     */
     private void registerRecipeTypes(GTCEuAPI.RegisterEvent<ResourceLocation, GTRecipeType> event) {
         RecipeTypes.init();
     }
 
-    /**
-     * Used to register your own new machines.
-     * Call init() from your Machine class(es) here
-     *
-     * @param event
-     */
     private void registerMachines(GTCEuAPI.RegisterEvent<ResourceLocation, MachineDefinition> event) {
         Multiblocks.init();
     }
 
-    /**
-     * Used to register your own new sounds
-     * Call init from your Sound class(es) here
-     *
-     * @param event
-     */
     public void registerSounds(GTCEuAPI.RegisterEvent<ResourceLocation, SoundEntry> event) {
         // CustomSounds.init();
     }
