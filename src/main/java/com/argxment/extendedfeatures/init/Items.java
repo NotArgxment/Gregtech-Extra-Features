1253d9d8cd2e15743a222e30e9c94dec7504b817
Usuario <therealxkorpion@gmail.com>
2026-06-27 15:15:49 -0300

    .

--- src/main/java/com/argxment/extendedfeatures/init/Items.java
+++ src/main/java/com/argxment/extendedfeatures/init/Items.java
@@ -124,7 +124,5 @@ public class Items {
             .tag(CustomTags.MAX_CIRCUITS)
             .register();
  */
-    public static void register(IEventBus eventBus) {
-
-    }
+    public static void register(IEventBus eventBus) {}
 }
